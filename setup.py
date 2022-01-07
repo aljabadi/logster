fccb279f3e0349dfb19e61844a09277b9acd7494
Al J Abadi <al.jal.abadi@gmail.com>
2022-01-07 23:48:06 +1100

    Create setup.cfg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="logster",
-    version="0.1",
+    version="0.1.0",
     description="Easy python console logging with colourful formatting",
     url="https://github.com/aljabadi/logster",
     author="Al J Abadi",
