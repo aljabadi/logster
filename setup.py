71a82856201ab1e37bcdd4b10dfb7ccd76785194
Al J Abadi <al.jal.abadi@gmail.com>
2022-01-08 00:15:52 +1100

    houskeeping

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name="logster",
     version="0.4.0",
     description="Easy python console logging with colourful formatting",
-    long_description = """logster is the logging one-liner solution for development 
-    environments. See example usage at 
-    https://github.com/aljabadi/logster/blob/master/README.md.
-    This is a development project. Feedback/enhancements are welcome.
-    """
+    long_description=(
+        "logster is the logging one-liner solution for development"
+        + "environments. See example usage at"
+        + "https://github.com/aljabadi/logster/blob/master/README.md."
+        + "This is a development project. Feedback/enhancements are welcome."
+    ),
     url="https://github.com/aljabadi/logster",
-    download_url="https://github.com/aljabadi/logster/archive/refs/tags/v0.3.0.tar.gz",
+    download_url="https://github.com/aljabadi/logster/archive/refs/tags/v0.5.0.tar.gz",
     author="Al J Abadi",
     author_email="al.jal.abadi@gmail.com",
     license="MIT",
